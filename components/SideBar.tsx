6f52e720205e3e2b3521852a802ce80197d011b9
rahmanshaukat35 <rahmanshaukat35@gmail.com>
2023-04-02 09:46:59 +0500

    layout

---
+++ components/SideBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import NewChat from './NewChat';
+
+function SideBar() {
+	return (
+		<div className='p-2 flex flex-col h-screen'>
+			<div className='flex-1'>
+				<div>
+					{/* New Chat */}
+					<NewChat />
+					<div>{/* ModelSelection */}</div>
+					{/* Map through the chatRows */}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default SideBar;
